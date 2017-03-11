cdafca725bac5a0e71aff0d45f40858fcc98fd6e
ianrae <ianrae88@gmail.com>
2017-03-11 07:26:15 -0500

    fix slf4j initial logging

---
+++ src/test/java/com/github/ianrae/dnalparse/systest/ErrorMessageTests.java
@@ -0,0 +1,28 @@
+package com.github.ianrae.dnalparse.systest;
+
+import static org.junit.Assert.*;
+
+import org.dnal.core.logger.Log;
+import org.junit.Test;
+
+public class ErrorMessageTests {
+
+	@Test
+	public void test() {
+		Log.log("test log1 - will show");
+		Log.debugLog("test log1 - won't show");
+		Log.debugLogging = true;
+		Log.debugLog("test log1 - will show");
+		Log.debugLogging = false; //reset
+	}
+
+	@Test
+	public void test2() {
+		Log.useSLFLogging = true;
+		Log.log("slf-test log1 - will show");
+		Log.debugLog("slf-test log1 - won't show");
+		Log.debugLogging = true;
+		Log.debugLog("slf-test log1 - will show");
+		Log.debugLogging = false; //reset
+	}
+}
