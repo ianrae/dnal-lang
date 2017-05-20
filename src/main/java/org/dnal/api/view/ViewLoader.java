ff38788a12d474085979bac902e323f7e36a42ba
ianrae <ianrae88@gmail.com>
2017-05-19 21:02:50 -0400

    viewloader

--- src/main/java/org/dnal/api/view/ViewLoader.java
+++ src/main/java/org/dnal/api/view/ViewLoader.java
@@ -17,30 +17,33 @@ public class ViewLoader extends ViewLoaderRendererBase {
 		public ViewLoader(DataSet ds) {
 			super(ds);
 		}
-		public DValue load(DValue source, DStructType structType) throws Exception {
+		public DValue load(DValue source, DStructType targetType) throws Exception {
 			if (!(source.getType() instanceof DViewType)) {
 				throw new IllegalArgumentException(String.format("source must be a DViewType: %s", source.getType().getName()));
 			}
 			DViewType viewType = (DViewType) source.getType();
-			DType sourceType = registry.getType(viewType.getRelatedTypeName());
 			
-			if (!(sourceType instanceof DStructType)) {
+			DType tmp = registry.getType(viewType.getRelatedTypeName());
+			if (!(tmp instanceof DStructType)) {
 				throw new IllegalArgumentException(String.format("can't find type: %s", viewType.getRelatedTypeName()));
 			}
+			DStructType intoType = (DStructType) tmp;
 
 			if (! viewType.getDirection().equals(ViewDirection.INBOUND)) {
 				throw new IllegalArgumentException(String.format("cannot render an inview: %s", viewType.getName()));
 			}
 
 			//source can be circle and sourceType can be shape
-			if (! sourceType.isAssignmentCompatible(sourceType)) {
+			//clazzShape.isAssignableFrom(clazzCir);
+			//so targetType can be a circ and sourceType can be a shape
+			if (! intoType.isAssignmentCompatible(targetType)) {
 				throw new IllegalArgumentException(String.format("type mismatch. view expects %s but got %s", viewType.getRelatedTypeName(), source.getType().getName()));
 			}
 
 			Transaction trans = ds.createTransaction();
 			Map<String,DValue> resultMap = new HashMap<>();
-			for(String targetFieldName: structType.getFields().keySet()) {
-				DType destType = structType.getFields().get(targetFieldName);
+			for(String targetFieldName: intoType.getFields().keySet()) {
+				DType destType = intoType.getFields().get(targetFieldName);
 				String srcField = viewType.getNamingMap().get(targetFieldName);
 				DValue sourceVal = source.asStruct().getField(srcField);
 
@@ -48,7 +51,7 @@ public class ViewLoader extends ViewLoaderRendererBase {
 				resultMap.put(targetFieldName, inner);
 			}
 
-			DValueImpl dval = new DValueImpl(structType, resultMap); 
+			DValueImpl dval = new DValueImpl(targetType, resultMap); 
 			return dval;
 		}
 
--- src/test/java/org/dnal/other/OtherTests.java
+++ src/test/java/org/dnal/other/OtherTests.java
@@ -5,8 +5,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class OtherTests {
+	
+	static class Shape {
+		public int x;
+		public int y;
+	}
+	
+	static class Circle extends Shape {
+		public int radius;
+	}
 
 	@Test
 	public void test() {
+		Class<?> clazzShape = Shape.class;
+		Class<?> clazzCir = Circle.class;
+		
+		boolean b = clazzShape.isAssignableFrom(clazzCir);
+		assertEquals(true, b);
+		b = clazzCir.isAssignableFrom(clazzShape);
+		assertEquals(false, b);
 	}
 }
