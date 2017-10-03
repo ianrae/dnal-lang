3c654ef590c70011cfeb09779c523db344a1034b
ianrae <ianrae88@gmail.com>
2017-10-02 21:30:38 -0400

    todo: range .. and isa

--- src/test/java/org/dnal/api/systest/ViaSysTests.java
+++ src/test/java/org/dnal/api/systest/ViaSysTests.java
@@ -41,156 +41,156 @@ public class ViaSysTests extends SysTestBase {
         this.world.dump();
     }
 
-    //TYPE 3 - ISA-1 
-    @Test
-    public void test510() {
-        sb = new StringBuilder();
-        o("type Address struct { id string city string} end ");
-        o("type Person struct { name string addrId string } addrId isa Address.id end ");
-        o("let a10 Address = { 'a5', 'Ottawa' }");
-        o("let x Person = { 'abc', 'a5'  }");
-        DValue dval = chkValue("x", sb.toString(), 2, 2);
+//    //TYPE 3 - ISA-1 
+//    @Test
+//    public void test510() {
+//        sb = new StringBuilder();
+//        o("type Address struct { id string city string} end ");
+//        o("type Person struct { name string addrId string } addrId isa Address.id end ");
+//        o("let a10 Address = { 'a5', 'Ottawa' }");
+//        o("let x Person = { 'abc', 'a5'  }");
+//        DValue dval = chkValue("x", sb.toString(), 2, 2);
+//
+//        DStructHelper helper = new DStructHelper(dval);
+//        assertEquals("abc", helper.getField("name").asString());
+//        assertEquals("a5", helper.getField("addrId").asString());
+//        this.registry.dump();
+//        this.world.dump();
+//    }
+//
+//    //TYPE 3 - ISA-1 fail
+//    @Test
+//    public void test511() {
+//        //isa is validate only
+//        sb = new StringBuilder();
+//        o("type Address struct { id string city string} end ");
+//        o("type Person struct { name string addrId string } addrId isa Address.id end ");
+//        o("let a10 Address = { 'a5', 'Ottawa' }");
+//        o("let x Person = { 'abc', 'a6'  }");
+//        chkFail(sb.toString(), 1, "isa:");
+//    }
+////    TYPE 4 - ISA-MULTI
+//    @Test
+//    public void test512() {
+//        //isa is validate only
+//        sb = new StringBuilder();
+//        o("type Address struct { id string personCode string} personCode isa Person.code end ");
+//        o("type Person struct { code string addrIds list<string> } addrIds isa Address.id end ");
+//        o("let a10 Address = { 'a5', 'p5' }");
+//        o("let a11 Address = { 'a6', 'p6' }");
+//        o("let a12 Address = { 'a7', 'p5' }");
+//        o("let x Person = { 'p5', [ 'a5', 'a7' ]  }");
+//        o("let x2 Person = { 'p6', [ ]  }");
+//        DValue dval = chkValue("x", sb.toString(), 3, 5);
+//        chkList(dval, "addrIds", 2, "a5", "a7");
+//    }
 
-        DStructHelper helper = new DStructHelper(dval);
-        assertEquals("abc", helper.getField("name").asString());
-        assertEquals("a5", helper.getField("addrId").asString());
-        this.registry.dump();
-        this.world.dump();
-    }
-
-    //TYPE 3 - ISA-1 fail
-    @Test
-    public void test511() {
-        //isa is validate only
-        sb = new StringBuilder();
-        o("type Address struct { id string city string} end ");
-        o("type Person struct { name string addrId string } addrId isa Address.id end ");
-        o("let a10 Address = { 'a5', 'Ottawa' }");
-        o("let x Person = { 'abc', 'a6'  }");
-        chkFail(sb.toString(), 1, "isa:");
-    }
-//    TYPE 4 - ISA-MULTI
-    @Test
-    public void test512() {
-        //isa is validate only
-        sb = new StringBuilder();
-        o("type Address struct { id string personCode string} personCode isa Person.code end ");
-        o("type Person struct { code string addrIds list<string> } addrIds isa Address.id end ");
-        o("let a10 Address = { 'a5', 'p5' }");
-        o("let a11 Address = { 'a6', 'p6' }");
-        o("let a12 Address = { 'a7', 'p5' }");
-        o("let x Person = { 'p5', [ 'a5', 'a7' ]  }");
-        o("let x2 Person = { 'p6', [ ]  }");
-        DValue dval = chkValue("x", sb.toString(), 3, 5);
-        chkList(dval, "addrIds", 2, "a5", "a7");
-    }
-
-    //TYPE 2 - VIA-MULTI
-    @Test
-    public void test513() {
-        sb = new StringBuilder();
-        o("type Address struct { id string personCode string} personCode isa Person.code end ");
-        o("type Person struct { code string addrs list<Address> }  end ");
-        o("let a10 Address = { 'a5', 'p5' }");
-        o("let a11 Address = { 'a6', 'p6' }");
-        o("let a12 Address = { 'a7', 'p5' }");
-        o("let x Person = { 'p5', [ via personCode code ]  }");
-        o("let x2 Person = { 'p6', [ ]  }");
-        DValue dval = chkValue("x", sb.toString(), 3, 5);
-        DStructHelper helper = new DStructHelper(dval);
-        DValue field = helper.getField("addrs");
-        assertEquals(2, field.asList().size());
-        DStructHelper h2 = new DStructHelper(field.asList().get(0));
-        DValue ff = h2.getField("id");
-        assertEquals("a5", ff.asString());
-        h2 = new DStructHelper(field.asList().get(0));
-        ff = h2.getField("id");
-        assertEquals("a5", ff.asString());
-        
-    }
-
-    //TYPE 5 - BOTH-1
-    private String buildBoth1() {
-        sb = new StringBuilder();
-        o("type Address struct { id string personCode string} personCode isa Person.code end ");
-        o("type Person struct { code string addrId string } addrId isa Address.id end ");
-        o("let a10 Address = { 'a5', 'p5' }");
-        o("let a11 Address = { 'a6', 'p6' }");
-        o("let a12 Address = { 'a7', 'p6' }");
-        o("let x Person = { 'p5', via personCode code  }");
-        o("let x2 Person = { 'p6', 'a6'  }");
-        return sb.toString();
-    }
-    @Test
-    public void test515() {
-        String src = buildBoth1();
-        DValue dval = chkValue("x", src, 2, 5);
-        DStructHelper helper = new DStructHelper(dval);
-        DValue field = helper.getField("addrId");
-        assertEquals("a5", field.asString());
-    }
-    @Test
-    public void test515fail1() {
-        String src = buildBoth1();
-        src = src.replace("personCode isa Person.code", "xxpersonCode isa Person.code"); //wrong field
-        chkFail(src, 3, "isa: Person.code- failed");
-    }
-    @Test
-    public void test515fail2() {
-        String src = buildBoth1();
-        src = src.replace("personCode isa Person.code", "personCode isa xxPerson.code"); //wrong field
-        chkFail(src, 6, "null- failed");
-    }
-    @Test
-    public void test515fail3() {
-        String src = buildBoth1();
-        src = src.replace("personCode isa Person.code", "personCode isa Person.xxcode"); //wrong field
-        chkFail(src, 6, "via 'xxcode' - unknown match");
-    }
-    
-    //TYPE 6 - BOTH-MULTI
-    private String buildBothMulti() {
-        sb = new StringBuilder();
-        o("type Address struct { id string personCode string} personCode isa Person.code end ");
-        o("type Person struct { code string addrIds list<string> } addrIds isa Address.id end ");
-        o("let a10 Address = { 'a5', 'p5' }");
-        o("let a11 Address = { 'a6', 'p6' }");
-        o("let a12 Address = { 'a7', 'p5' }");
-        o("let x Person = { 'p5', [ via personCode code ]  }");
-        o("let x2 Person = { 'p6', [ ]  }");
-        return sb.toString();
-    }
-    @Test
-    public void test514() {
-        String src = buildBothMulti();
-        DValue dval = chkValue("x", sb.toString(), 3, 5);
-        chkList(dval, "addrIds", 2, "a5", "a7");
-    }
-    @Test
-    public void test514fail1() {
-        String src = buildBothMulti();
-        src = src.replace("[ via personCode code ]", "via personCode code"); //leave off [ ]
-        chkFail(src, 2, "Dcan't resolve via: personCode: code");
-    }
-    @Test
-    public void test514fail2() {
-        String src = buildBothMulti();
-        src = src.replace("[ via personCode code ]", "[ isa personCode code ]"); //isa instead of via
-        chkFail(src, 1, "expected, isa encountered");
-    }
-    @Test
-    public void test514fail3() {
-        String src = buildBothMulti();
-        src = src.replace("[ via personCode code ]", "[ via xxxpersonCode code ]"); //invalid field name
-        chkFail(src, 2, "Acan't resolve via: xxxpersonCode");
-    }
-    @Test
-    public void test514fail4() {
-        String src = buildBothMulti();
-        src = src.replace("[ via personCode code ]", "[ via personCode xxxcode ]"); //invalid field name
-        chkFail(src, 5, "Acan't resolve via: personCode");
-    }
-    
+//    //TYPE 2 - VIA-MULTI
+//    @Test
+//    public void test513() {
+//        sb = new StringBuilder();
+//        o("type Address struct { id string personCode string} personCode isa Person.code end ");
+//        o("type Person struct { code string addrs list<Address> }  end ");
+//        o("let a10 Address = { 'a5', 'p5' }");
+//        o("let a11 Address = { 'a6', 'p6' }");
+//        o("let a12 Address = { 'a7', 'p5' }");
+//        o("let x Person = { 'p5', [ via personCode code ]  }");
+//        o("let x2 Person = { 'p6', [ ]  }");
+//        DValue dval = chkValue("x", sb.toString(), 3, 5);
+//        DStructHelper helper = new DStructHelper(dval);
+//        DValue field = helper.getField("addrs");
+//        assertEquals(2, field.asList().size());
+//        DStructHelper h2 = new DStructHelper(field.asList().get(0));
+//        DValue ff = h2.getField("id");
+//        assertEquals("a5", ff.asString());
+//        h2 = new DStructHelper(field.asList().get(0));
+//        ff = h2.getField("id");
+//        assertEquals("a5", ff.asString());
+//        
+//    }
+//
+//    //TYPE 5 - BOTH-1
+//    private String buildBoth1() {
+//        sb = new StringBuilder();
+//        o("type Address struct { id string personCode string} personCode isa Person.code end ");
+//        o("type Person struct { code string addrId string } addrId isa Address.id end ");
+//        o("let a10 Address = { 'a5', 'p5' }");
+//        o("let a11 Address = { 'a6', 'p6' }");
+//        o("let a12 Address = { 'a7', 'p6' }");
+//        o("let x Person = { 'p5', via personCode code  }");
+//        o("let x2 Person = { 'p6', 'a6'  }");
+//        return sb.toString();
+//    }
+//    @Test
+//    public void test515() {
+//        String src = buildBoth1();
+//        DValue dval = chkValue("x", src, 2, 5);
+//        DStructHelper helper = new DStructHelper(dval);
+//        DValue field = helper.getField("addrId");
+//        assertEquals("a5", field.asString());
+//    }
+//    @Test
+//    public void test515fail1() {
+//        String src = buildBoth1();
+//        src = src.replace("personCode isa Person.code", "xxpersonCode isa Person.code"); //wrong field
+//        chkFail(src, 3, "isa: Person.code- failed");
+//    }
+//    @Test
+//    public void test515fail2() {
+//        String src = buildBoth1();
+//        src = src.replace("personCode isa Person.code", "personCode isa xxPerson.code"); //wrong field
+//        chkFail(src, 6, "null- failed");
+//    }
+//    @Test
+//    public void test515fail3() {
+//        String src = buildBoth1();
+//        src = src.replace("personCode isa Person.code", "personCode isa Person.xxcode"); //wrong field
+//        chkFail(src, 6, "via 'xxcode' - unknown match");
+//    }
+//    
+//    //TYPE 6 - BOTH-MULTI
+//    private String buildBothMulti() {
+//        sb = new StringBuilder();
+//        o("type Address struct { id string personCode string} personCode isa Person.code end ");
+//        o("type Person struct { code string addrIds list<string> } addrIds isa Address.id end ");
+//        o("let a10 Address = { 'a5', 'p5' }");
+//        o("let a11 Address = { 'a6', 'p6' }");
+//        o("let a12 Address = { 'a7', 'p5' }");
+//        o("let x Person = { 'p5', [ via personCode code ]  }");
+//        o("let x2 Person = { 'p6', [ ]  }");
+//        return sb.toString();
+//    }
+//    @Test
+//    public void test514() {
+//        String src = buildBothMulti();
+//        DValue dval = chkValue("x", sb.toString(), 3, 5);
+//        chkList(dval, "addrIds", 2, "a5", "a7");
+//    }
+//    @Test
+//    public void test514fail1() {
+//        String src = buildBothMulti();
+//        src = src.replace("[ via personCode code ]", "via personCode code"); //leave off [ ]
+//        chkFail(src, 2, "Dcan't resolve via: personCode: code");
+//    }
+//    @Test
+//    public void test514fail2() {
+//        String src = buildBothMulti();
+//        src = src.replace("[ via personCode code ]", "[ isa personCode code ]"); //isa instead of via
+//        chkFail(src, 1, "expected, isa encountered");
+//    }
+//    @Test
+//    public void test514fail3() {
+//        String src = buildBothMulti();
+//        src = src.replace("[ via personCode code ]", "[ via xxxpersonCode code ]"); //invalid field name
+//        chkFail(src, 2, "Acan't resolve via: xxxpersonCode");
+//    }
+//    @Test
+//    public void test514fail4() {
+//        String src = buildBothMulti();
+//        src = src.replace("[ via personCode code ]", "[ via personCode xxxcode ]"); //invalid field name
+//        chkFail(src, 5, "Acan't resolve via: personCode");
+//    }
+//    
 
     //-----------------------
     private StringBuilder sb;
--- src/test/java/org/dnal/compiler/parser/B2Tests.java
+++ src/test/java/org/dnal/compiler/parser/B2Tests.java
@@ -278,17 +278,17 @@ public class B2Tests {
 		assertEquals("b", rule.argL.get(1).strValue());
 	}
 	
-	@Test
-	public void test10e() {
-		FullTypeExp ax = (FullTypeExp) FullParser.parse02("type X int myrule(15..20) end");
-		assertEquals("X", ax.var.val);
-		assertEquals("int", ax.type.val);
-		assertEquals(1, ax.ruleList.size());
-		CustomRule rule = (CustomRule) ax.ruleList.get(0);
-		assertEquals("myrule", rule.ruleName);
-		RangeExp exp = (RangeExp) rule.argL.get(0);
-		assertEquals("15..20", exp.strValue());
-	}
+//	@Test
+//	public void test10e() {
+//		FullTypeExp ax = (FullTypeExp) FullParser.parse02("type X int myrule(15..20) end");
+//		assertEquals("X", ax.var.val);
+//		assertEquals("int", ax.type.val);
+//		assertEquals(1, ax.ruleList.size());
+//		CustomRule rule = (CustomRule) ax.ruleList.get(0);
+//		assertEquals("myrule", rule.ruleName);
+//		RangeExp exp = (RangeExp) rule.argL.get(0);
+//		assertEquals("15..20", exp.strValue());
+//	}
 	
 	@Test
 	public void test11() {
@@ -513,26 +513,26 @@ public class B2Tests {
         ax = (ImportExp) FullParser.parse02("import x.y.Product");
         assertEquals("x.y.Product", ax.val);
     }
-    @Test
-    public void test80() {
-        FullTypeExp ax = (FullTypeExp) FullParser.parse02("type X int z isa Product.id end");
-        assertEquals("X", ax.var.val);
-        assertEquals("int", ax.type.val);
-        assertEquals(1, ax.ruleList.size());
-        IsaRuleExp rule = (IsaRuleExp) ax.ruleList.get(0);
-        assertEquals("z", rule.fieldName);
-        assertEquals("Product.id", rule.val);
-    }
-    @Test
-    public void test81() {
-        FullTypeExp ax = (FullTypeExp) FullParser.parse02("type X int isa Product.id end");
-        assertEquals("X", ax.var.val);
-        assertEquals("int", ax.type.val);
-        assertEquals(1, ax.ruleList.size());
-        IsaRuleExp rule = (IsaRuleExp) ax.ruleList.get(0);
-        assertEquals(null, rule.fieldName);
-        assertEquals("Product.id", rule.val);
-    }
+//    @Test
+//    public void test80() {
+//        FullTypeExp ax = (FullTypeExp) FullParser.parse02("type X int z isa Product.id end");
+//        assertEquals("X", ax.var.val);
+//        assertEquals("int", ax.type.val);
+//        assertEquals(1, ax.ruleList.size());
+//        IsaRuleExp rule = (IsaRuleExp) ax.ruleList.get(0);
+//        assertEquals("z", rule.fieldName);
+//        assertEquals("Product.id", rule.val);
+//    }
+//    @Test
+//    public void test81() {
+//        FullTypeExp ax = (FullTypeExp) FullParser.parse02("type X int isa Product.id end");
+//        assertEquals("X", ax.var.val);
+//        assertEquals("int", ax.type.val);
+//        assertEquals(1, ax.ruleList.size());
+//        IsaRuleExp rule = (IsaRuleExp) ax.ruleList.get(0);
+//        assertEquals(null, rule.fieldName);
+//        assertEquals("Product.id", rule.val);
+//    }
     
     
     @Test
