53439143573e0e53d60e011fdece5ce080564729
ianrae <ianrae88@gmail.com>
2018-11-21 02:13:32 -0500

    fix

--- src/main/java/org/dnal/api/impl/AddObserver.java
+++ src/main/java/org/dnal/api/impl/AddObserver.java
@@ -9,44 +9,48 @@ import org.dnal.core.repository.WorldAdder;
 
 public class AddObserver {
 
-    private WorldAdder adder;
+	private WorldAdder adder;
 
-    public AddObserver(WorldAdder adder) {
-        this.adder = adder;
-    }
+	public AddObserver(WorldAdder adder) {
+		this.adder = adder;
+	}
 
-    //will not handle circular references!!
-    public void observe(DValue dval) {
-        doObserve(dval, false);
-    }
-    
-    private void doObserve(DValue dval, boolean doAdd) {
-        if (doAdd) {
-            adder.valueAdded(dval);
-        }
-        
-        if (dval.getType().isScalarShape()) {
-        	//scalars don't have any sub-values
-        } else if (dval.getType().isStructShape()) {
-            DStructHelper helper = new DStructHelper(dval);
-            for(String fieldName: helper.getFieldNames()) {
-                DValue field = helper.getField(fieldName);
-                //ignore optional fields
-                if (field != null) {
-                	doObserve(field, true); //**recursion**
-                }
-            }
-        } else if (dval.getType().isShape(Shape.ENUM)) {
-            DStructHelper helper = new DStructHelper(dval);
-            for(String fieldName: helper.getFieldNames()) {
-                DValue field = helper.getField(fieldName);
-               	doObserve(field, true); //**recursion**
-            }
-        } else if (dval.getType().isShape(Shape.LIST)) {
-            List<DValue> list = dval.asList();
-            for(DValue el: list) {
-                doObserve(el, true); //**recursion**
-            }
-        }
-    }
+	//will not handle circular references!!
+	public void observe(DValue dval) {
+		doObserve(dval, false);
+	}
+
+	private void doObserve(DValue dval, boolean doAdd) {
+		if (doAdd) {
+			if (dval.getType().isScalarShape()) {
+				//scalars don't need to be added. only structs (for via and unique)
+			} else {
+				adder.valueAdded(dval);
+			}
+		}
+
+		if (dval.getType().isScalarShape()) {
+			//scalars don't have any sub-values
+		} else if (dval.getType().isStructShape()) {
+			DStructHelper helper = new DStructHelper(dval);
+			for(String fieldName: helper.getFieldNames()) {
+				DValue field = helper.getField(fieldName);
+				//ignore optional fields
+				if (field != null) {
+					doObserve(field, true); //**recursion**
+				}
+			}
+		} else if (dval.getType().isShape(Shape.ENUM)) {
+			DStructHelper helper = new DStructHelper(dval);
+			for(String fieldName: helper.getFieldNames()) {
+				DValue field = helper.getField(fieldName);
+				doObserve(field, true); //**recursion**
+			}
+		} else if (dval.getType().isShape(Shape.LIST)) {
+			List<DValue> list = dval.asList();
+			for(DValue el: list) {
+				doObserve(el, true); //**recursion**
+			}
+		}
+	}
 }
